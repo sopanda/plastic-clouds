1289783b4905cbb9b18253b8cf2b5956e6393078
sopanda <hushchabogdan@gmail.com>
2017-05-25 23:02:49 +0200

    Optimized sprites for Background and Added new sprite - sky. Also added script for Camera(now it follows our hero) Some changes for ground and physics

---
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject hero;
+    private PlayerScore heroScore;
+
+    private float minX = 0f, maxX = 204f;
+
+	void Awake ()
+    {
+        hero = GameObject.Find("HERO");
+        heroScore = hero.GetComponent<PlayerScore>();
+	}
+	
+	void Update ()
+    {
+        FollowHero();
+	}
+
+    private void FollowHero()
+    {
+        if(heroScore.isAlive)
+        {
+            Vector3 tmp = transform.position;
+
+            tmp.x = hero.transform.position.x;
+
+            if(tmp.x < minX)
+            {
+                tmp.x = minX;
+            }
+
+            if (tmp.x > maxX)
+            {
+                tmp.x = maxX;
+            }
+
+            tmp.y = hero.transform.position.y + 3f;
+
+            transform.position = tmp;
+        }
+    }
+}//class
---
+++ Assets/Scripts/Hero Scripts/PlayerScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    public bool isAlive;
+
+	void Awake ()
+    {
+        isAlive = true;	
+	}
+
+}//class
