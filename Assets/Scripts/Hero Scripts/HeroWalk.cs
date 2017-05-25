270765dbfcde5e544a7dc62f93de80e05de56ce7
sopanda <hushchabogdan@gmail.com>
2017-05-25 19:51:19 +0200

    Added Animation for Hero. Added jumping for Hero. Added physics for jumping and moving.

--- Assets/Scripts/Hero Scripts/HeroWalk.cs
+++ Assets/Scripts/Hero Scripts/HeroWalk.cs
@@ -7,16 +7,22 @@ public class HeroWalk : MonoBehaviour
     private float speed = 30f, maxVelocity = 4f;
 
     private Rigidbody2D myBody;
+    private Animator anim;
+
+    private float jumpForce = 500f;
+    private bool isGrounded = false;
 
 	void Awake ()
     {
         myBody = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
 	}
 	
 
 	void FixedUpdate ()
     {
         HeroWalkKeyboard();
+        Jump();
 	}
 
     private void HeroWalkKeyboard()
@@ -35,6 +41,12 @@ public class HeroWalk : MonoBehaviour
             {
                 forceX = speed;
             }
+
+            Vector3 temp = transform.localScale; // took the local scale
+            temp.x = 9.160614f; 
+            transform.localScale = temp; //set new scale
+
+            anim.SetBool("Walk", true);
         }
         else if(h < 0)
         {
@@ -42,8 +54,38 @@ public class HeroWalk : MonoBehaviour
             {
                 forceX = -speed;
             }
+
+            Vector3 temp = transform.localScale; // took the local scale
+            temp.x = -9.160614f;
+            transform.localScale = temp; //set new scale
+
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
         }
 
         myBody.AddForce(new Vector2(forceX, 0));
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(isGrounded)
+            {
+                isGrounded = false;
+                myBody.AddForce(new Vector2(0, jumpForce));
+            } 
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 } //class
